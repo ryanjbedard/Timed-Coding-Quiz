c153253e90d53669f64a0965debb12ee86fde574
rbedard <ryanjbedard@gmail.com>
2023-11-12 20:59:01 -0800

    Add variables for Questions 1-4 and answers.

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,46 +1,34 @@
-var timerEl = document.getElementById('Timer');
-var startQuiz =document.querySelector('startBtn') 
+var timerEl = document.getElementById("Timer");
+var startQuiz = document.querySelector("startBtn");
 
-function startQuiz() {
-    timerID = setInterval(clockTick, 1000);
-    timerEl.textContent = 'Time';
-    var landingScreenEl = document.getElementById("start-screen");
-    landingScreenEl.setAttribute('class','hide');
-    questionsEl.removeAttribute("class");
-    getQuestion();
-}
-function getQuestion() {
-    var currentQuestion = questions[currentQuestionIndex];
-    var promptEl = document.getElementById("questions-words");
-    promptEl.textContent = currentQuestion.prompt;
-    choicesEl.innerHTML = "";
-    currentQuestion.options.forEach(
-        fucntion (choice, i) {
-            var choiceBtn = document.createElement("button");
-            choiceBtn.setAttribute("value", choice);
-            choiceBtn.textContent = i + 1 + ". " + choice;
-            choiceBtn.onclick = questionClick;
-            ChoicesE1.appendChild(choiceBtn);
-
-        }
-    );
-}
+startQuiz.addEventListener("click", function () {
+    var questionOne =
+    var questionTwo = [""]
+    var questionThree = [""]
+    var questionFour = [""]
+    var questionOneAnswers = [""]
+    var questionTwoAnswers = [""]
+    var questionThreeAnswers = [""]
+    var questionFourAnswers = [""]
 
 
-// function timer() {
-//     var timeLeft = 90;
+  
+    function timer() {
+    var timerEl = 90;
 
-//     var timeInterval = setInterval(function(){
-//         timeLeft--;
-//         timerEl.textContent = "Time: " + timeLeft;
+    var timeInterval = setInterval(function () {
+      timerEl--;
+      timerEl.textContent = "Time: " + timerEl;
 
-//         if (timeLeft === 0){
-//             clearInterval(timeInterval);
-//         }
-//     }, 1000);
-// }
+      if (timerEl === 0) {
+        clearInterval(timeInterval);
+      }
+      if (answer !== correct) {
+        timerEl - 10;
+      }
+    }, 1000);
 
-// startQuiz.addEventListener("click", function() {
-
-// })
-// timer();
+  }
+  function
+});
+startQuiz();
